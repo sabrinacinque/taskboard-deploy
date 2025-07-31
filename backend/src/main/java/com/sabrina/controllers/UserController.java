6c80144307ac762eac538a26028bce1076453f05
Sabrina Cinque <cinque.sabrina@gmail.com>
2025-07-31 20:01:24 +0200

    cambiato enviroment per back nuovo

--- backend/src/main/java/com/sabrina/controllers/UserController.java
+++ backend/src/main/java/com/sabrina/controllers/UserController.java
@@ -3,8 +3,6 @@ package com.sabrina.controllers;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
